3f4c2c3ab8ca4065e0da4c664be13fc917fbeaef
faruqsoft <omarfaruqngdc@gmail.com>
2024-12-05 14:05:16 +0600

    add try catch blog

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
+//
+// const URL = "https://cat-fact.herokuapp.com/facts"
+//
+// const factPara = document.querySelector("#collect")
+// const btn = document.querySelector("#btn");
+//
+//
+//
+// const getFacts = async () => {
+//     console.log("getting data.....");
+//
+//     let response = await fetch(URL);
+//     console.log(response);
+//     let facts = await response.json();
+//     console.log(facts);
+//     factPara.innerText = facts[0].text;
+// };
+// btn.addEventListener("click", getFacts);
 
-const URL = "https://cat-fact.herokuapp.com/facts"
 
-const factPara = document.querySelector("#collect")
+const URL = "https://cat-fact.herokuapp.com/facts";
+
+const factPara = document.querySelector("#collect");
 const btn = document.querySelector("#btn");
-
-
 
 const getFacts = async () => {
     console.log("getting data.....");
-
-    let response = await fetch(URL);
-    console.log(response);
-    let facts = await response.json();
-    console.log(facts);
-    factPara.innerText = facts[0].text;
+    try {
+        let response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let facts = await response.json();
+        console.log(facts);
+        factPara.innerText = facts[0].text;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        factPara.innerText = "Failed to fetch cat facts. Please try again later.";
+    }
 };
+
+btn.addEventListener("click", getFacts);
+
+
 
 // promise chain
 // function getFacts(){
@@ -26,7 +53,7 @@ const getFacts = async () => {
 //         })
 //
 // }
-btn.addEventListener("click", getFacts);
+
 
 
 
